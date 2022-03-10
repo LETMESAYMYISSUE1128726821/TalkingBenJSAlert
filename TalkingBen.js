d9aa05eac0acb1e551b7986d67bc53f7993fb848
LETMESAYMYISSUE1128726821 <75098003+LETMESAYMYISSUE1128726821@users.noreply.github.com>
2022-03-09 22:38:44 -0500

    Add files via upload

---
+++ TalkingBen.js
@@ -0,0 +1,14 @@
+prompt('Ben?')
+var ran = math.random() * (4 - 1 + 1)) + 1 
+if (ran = 1) {
+  alert("yes")
+}
+if (ran = 2) {
+  alert("no")
+}
+if (ran = 3) {
+  alert("BLEH")
+}
+if (ran = 4) {
+  alert("HA HA HA")
+}
